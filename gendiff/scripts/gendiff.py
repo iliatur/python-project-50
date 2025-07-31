854cbfc8abaa98e829b8d2461fcf78539a3b9594
Ilia Turygin <turygin_ia@mail.ru>
2025-07-31 11:00:18 +0200

    fix gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,11 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="gendiff",
+        usage="%(prog)s [-h] [-f FORMAT] first_file second_file",
+        description="Compares two configuration files and shows a difference.",
+    )
+        
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.print_help()
