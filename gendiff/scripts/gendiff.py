4b9906bb80287a009a2125f5f6322037a5e289f3
Ilia Turygin <turygin_ia@mail.ru>
2025-08-01 08:04:55 +0200

    update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,21 @@ def main():
         usage="%(prog)s [-h] [-f FORMAT] first_file second_file",
         description="Compares two configuration files and shows a difference.",
     )
-        
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+
+    parser.add_argument("first_file", nargs="+")
+    parser.add_argument("second_file", nargs="+")
+
+    parser.add_argument(
+        "-f", "--format", 
+        nargs="?", 
+        default='stylish', 
+        help="set format of output"
+    )
+    args = parser.parse_args()
+
+    path1 = args.first_file[0]
+    path2 = args.second_file[0]
+    formatter = args.format
+    
+    result = generate_diff(path1, path2, formatter)
+    print(result)
