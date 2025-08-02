9d3bd139af3f6dadf7b3f467790af4066eefe507
Ilia Turygin <turygin_ia@mail.ru>
2025-08-02 07:25:18 +0200

    fix __init__.py

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,8 +1,14 @@
 from .json import format_json
+from .plain import format_plain
+from .stylish import format_stylish
 
 
 def get_formatter(format_name):
     match format_name:
+        case 'stylish':
+            return format_stylish
+        case 'plain':
+            return format_plain
         case 'json':
             return format_json
         case _:
