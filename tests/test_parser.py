444a4ba0fcfc1b499da7ab47bc043d79029cc709
Ilia Turygin <turygin_ia@mail.ru>
2025-08-02 07:14:19 +0200

    add test_parser.py

---
+++ tests/test_parser.py
@@ -0,0 +1,22 @@
+import os
+
+from gendiff.parser import parse_file
+
+
+def test_parse_json():
+    path = os.path.join("tests", "test_data", "file1.json")
+    print(path)
+    result = parse_file(path)
+
+    assert result == {
+        "host": "hexlet.io",
+        "timeout": 50,
+        "proxy": "123.234.53.22",
+        "follow": False,
+    }
+
+
+def test_parse_yaml():
+    path = os.path.join("tests", "test_data", "file2.json")
+    result = parse_file(path)
+    assert result == {"timeout": 20, "verbose": True, "host": "hexlet.io"}
